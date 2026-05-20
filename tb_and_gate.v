ad558129f3fed5255f2cc33ebb2b71c799d6c046
Priyankaendluri <endlurupriyanka@gmail.com>
2026-05-20 17:27:13 +0530

    Add testbench for AND gate functionality

---
+++ tb_and_gate.v
@@ -0,0 +1,17 @@
+module tb_and_gate;
+  reg a, b;
+  wire y;
+  
+  and_gate uut(.a(a), .b(b), .y(y));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+    $display("Simulation done!");
+    $finish;
+  end
+endmodule
